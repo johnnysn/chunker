b1e6653c308f8d4f9a1edccb7776aa2ad717a5d0
johnnysn <johnnysn@gmail.com>
2024-05-17 13:02:43 -0300

    Endpoint for chunking a raw text

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
+from flask import request
+from flask_cors import CORS 
+# import time
+import re
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
@@ -8,6 +13,7 @@ def home():
 
 
 methods = [
+  {"id": "paragraphs", "name": "Split by paragraph", "description": "Split text by paragraphs"},
   {"id": "characters_count", "name": "Character count split", "description": "Split text by character count"},
   {"id": "sentences", "name": "Sentences split", "description": "Split text by groups of sentences"},
 ]
@@ -15,9 +21,24 @@ methods = [
 
 @app.route("/methods", methods=['GET'])
 def get_methods():
-  response = jsonify(methods)
-  response.headers.add("Access-Control-Allow-Origin", "*")
-  return response
+  return jsonify(methods)
+
+
+@app.route("/chunks/raw", methods=['POST'])
+def chunks_raw():
+  method_id = request.json['methodId']
+  text = request.json['text']
+  text_chunks = re.split(r'\n\s*', text)
+  chunks = [
+    {
+      "number": i + 1,
+      "text": text,
+    } for i, text in enumerate(text_chunks)
+  ]
+  # time.sleep(5)
+  return jsonify({
+    "chunks": chunks,
+  })
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def main():
-  print("Hello, shit")
+  print("Hello, dude")
 
 
 if __name__ == "__main__":
